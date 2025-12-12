a529a6cd4ada465dfcb9c6978cc62c7f913204b4
liyan <liyangeneral@gmail.com>
2025-12-12 00:26:02 -0800

    state part

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -1,17 +1,30 @@
-import PropTypes from "prop-types";
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import './Student.css';
 
 const Student = (props) => {
-  return (
-    <ul>
-      <li>Nickname: {props.name}</li>
-      <li>Email: {props.email}</li>
-    </ul>
-  );
+    const [isPresent, setIsPresent] = useState(false);
+
+    const togglePresence = () => {
+        setIsPresent(!isPresent);
+    };
+
+    const nameColor = isPresent ? 'green' : 'red';
+
+    return (
+        <div>
+            <ul>
+                <li className={nameColor}>Nickname: {props.name}</li>
+                <li>Email: {props.email}</li>
+            </ul>
+            <button onClick={togglePresence}>Toggle if {props.name} is present</button>
+        </div>
+    );
 };
 
 Student.propTypes = {
-  name: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired
 };
 
 export default Student;
