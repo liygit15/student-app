d118e8f8af3ae5837218dce983c85dbf70265397
liyan <liyangeneral@gmail.com>
2025-12-12 00:17:55 -0800

    add proptype

---
+++ src/components/ClassInfo.jsx
@@ -0,0 +1,13 @@
+const ClassInfo = (props) => {
+  return (
+    <section>
+      <h2>Class Information</h2>
+      <ul>
+        <li>Name: Team Semicolons</li>
+        <li>Number of members: {props.memberCount}</li>
+      </ul>
+    </section>
+  );
+};
+
+export default ClassInfo;
